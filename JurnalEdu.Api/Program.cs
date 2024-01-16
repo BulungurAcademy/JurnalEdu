932900731fec38141aecf4f1b2a51ccbb13eed01
ShohruzBobobekov <shohruzbobobekov505@gmail.com>
2024-01-17 01:17:37 +0500

    Add project files.

---
+++ JurnalEdu.Api/Program.cs
@@ -0,0 +1,28 @@
+
+namespace JurnalEdu.Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+            });
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
+}
