306bd4f21c5c232af07e72c259a5b3f851cf9682
Javohir Jumanov <jumanovjavohir49@gmail.com>
2024-01-22 17:34:02 +0500

    add

--- JurnalEdu.Core/Constants/TableNames.cs
+++ JurnalEdu.Core/Constants/TableNames.cs
@@ -2,8 +2,12 @@
 public static class TableNames
 {
     #region INFORMATION
+    public static string Country = "Country";
     public static string Region = "Region";
     public static string District = "District";
+    public static string Organization = "Organization";
+    public static string Nationality = "Nationality";
+    public static string Citizenship = "Citizenship";
     #endregion
 
     #region SYSTEM
