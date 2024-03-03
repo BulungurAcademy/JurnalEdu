a42236e6fad1d5a65bbcc8654cf32a9eeb1f948a
ShohruzBobobekov <shohruzbobobekov505@gmail.com>
2024-03-03 21:24:52 +0500

    base IEntity and some implementations

---
+++ JurnalEdu.Domain/Models/Base/Auditing/Concrete/CreationAuditedEntity.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using JurnalEdu.Domain.Models.Base.BaseEntities;
+
+namespace JurnalEdu.Domain.Models.Base.Auditing;
+
+/// <summary>
+/// A shortcut of <see cref="CreationAuditedEntity{TPrimaryKey}"/> for most used primary key type (<see cref="int"/>).
+/// </summary>
+public abstract class CreationAuditedEntity : CreationAuditedEntity<int>, IEntity
+{
+
+}
+
+/// <summary>
+/// This class can be used to simplify implementing <see cref="ICreationAudited"/>.
+/// </summary>
+/// <typeparam name="TPrimaryKey">Type of the primary key of the entity</typeparam>
+public abstract class CreationAuditedEntity<TPrimaryKey> : Entity<TPrimaryKey>, ICreationAudited
+{
+    /// <summary>
+    /// Creation time of this entity.
+    /// </summary>
+    public virtual DateTime CreationTime { get; set; }
+
+    /// <summary>
+    /// Creator of this entity.
+    /// </summary>
+    public virtual long? CreatorUserId { get; set; }
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    protected CreationAuditedEntity()
+    {
+        CreationTime = DateTime.Now;
+    }
+}
+
+/// <summary>
+/// This class can be used to simplify implementing <see cref="ICreationAudited{TUser}"/>.
+/// </summary>
+/// <typeparam name="TPrimaryKey">Type of the primary key of the entity</typeparam>
+/// <typeparam name="TUser">Type of the user</typeparam>
+public abstract class CreationAuditedEntity<TPrimaryKey, TUser> : CreationAuditedEntity<TPrimaryKey>, ICreationAudited<TUser>
+    where TUser : IEntity<long>
+{
+    /// <summary>
+    /// Reference to the creator user of this entity.
+    /// </summary>
+    [ForeignKey("CreatorUserId")]
+    public virtual TUser CreatorUser { get; set; }
+}
---
+++ JurnalEdu.Domain/Models/Base/BaseEntities/IMayHaveTenant.cs
@@ -0,0 +1,12 @@
+﻿namespace JurnalEdu.Domain.Models.Base.BaseEntities;
+
+/// <summary>
+/// Implement this interface for an entity which may optionally have TenantId.
+/// </summary>
+public interface IMayHaveTenant
+{
+    /// <summary>
+    /// TenantId of this entity.
+    /// </summary>
+    int? TenantId { get; set; }
+}
